736e6d8ec6665022ee71a53dcd78ab14ccd3a5a0
Vitor Pellegatti <vkenzofp@gmail.com>
2023-07-24 16:19:19 -0300

    Separating logic from the main + its error handling

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::{env, fs, process};
+use std::error::Error;
 
 fn main() {
 
@@ -12,11 +13,10 @@ fn main() {
     println!("Searching for {}", config.query);
     println!("In file {}", config.file_path);
 
-    let contents = fs::read_to_string(config.file_path)
-        .expect("Should have been able to read the file.");
-
-
-    println!("With text:\n{contents}");
+    if let Err(e) = run(config) {
+        println!("Application error: {e}");
+        process::exit(1);
+    }
 }
 
 struct Config {
@@ -35,4 +35,12 @@ impl Config {
 
         Ok(Config { query, file_path })
     }
+}
+
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.file_path)?;
+
+    println!("With text:\n{contents}");
+
+    Ok(())
 }
